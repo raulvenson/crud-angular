9d3bc74a59b6d6d0c01046e1294ec4f4445be6c5
raul-venson-martinello <informatica16@martinello.com.br>
2023-01-04 04:00:09 -0400

    Finalizando primeira parte do frontend do projeto.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,26 +1,36 @@
-import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+import { CoursesService } from '../services/courses.service';
+import { Course } from './../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-  courses: Course[] = [
-    {
-      _id: '1', name: 'Angular', category: 'front-end'
-    }
-  ];
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category'];
 
-  constructor(){
-    // this.courses = [];
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar dados da api');
+        return of([]);
+      })
+    );
   }
-  ngOnInit(): void {
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
-
-
-
+  ngOnInit(): void {}
 }
